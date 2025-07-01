ab9dfc346aa30c2cfe27ed8c4dc363718f4cea8f
unknown <luiscesar.munoz.cervantes.upiit@gmail.com>
2025-07-01 17:33:16 -0600

    name plans

--- src/Store/StorePage.js
+++ src/Store/StorePage.js
@@ -45,7 +45,7 @@ const StorePage = () => {
     console.log('Iniciando compra de suscripción:', plan);
     
     const priceId = PRICE_IDS[plan.id];
-    console.log('Price ID para el plan:', priceId);
+    console.log(plan.name ,'nam plan ');
     
     if (!priceId) {
       alert('Plan no configurado correctamente');
